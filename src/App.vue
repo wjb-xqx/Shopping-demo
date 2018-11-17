75a75ff6f2e7a3b3d5737fc059f716423a1ff0e6
xqx <1025529920@qq.com>
2018-11-17 13:27:09 +0800

    home组件轮播和6宫格完成

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,57 @@
   <div class="app-container">
    <!-- 顶部header区域 -->
   <mt-header fixed title="必要"></mt-header>
-   <!-- 中间的路由router-view区域 -->
 
-   <!-- 底部Tabber 区域 -->
-  <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
-				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-				<span class="mui-tab-label">消息</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">通讯录</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">设置</span>
-			</a>
-	</nav>
-   <h1>666</h1>
+   <!-- 中间的路由router-view区域 -->
+		<transition>
+			<router-view></router-view>
+		</transition>
+
+  		 <!-- 底部Tabber 区域 -->
+  		<nav class="mui-bar mui-bar-tab">
+					<router-link class="mui-tab-item" to="/home">
+						<span class="mui-icon mui-icon-home"></span>
+						<span class="mui-tab-label">首页</span>
+					</router-link>
+					<router-link class="mui-tab-item" to="/member">
+						<span class="mui-icon mui-icon-contact"></span>
+						<span class="mui-tab-label">会员</span>
+					</router-link>
+					<router-link class="mui-tab-item" to="/shopcar">
+						<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
+						<span class="mui-tab-label">购物车</span>
+					</router-link>
+					<router-link class="mui-tab-item" to="/search">
+						<span class="mui-icon mui-icon-search"></span>
+						<span class="mui-tab-label">搜索</span>
+					</router-link>
+			</nav>
   </div>
 </template>
 
 <script>
-
+			
 </script>
 
 // 局部样式
 <style lang="scss" scoped>  
   .app-container {
-    padding-top:40px;
-  }
+		padding-top:40px;
+		overflow-x: hidden;
+	}
+	.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
 // 入口文件
 import Vue from 'vue'
+
+/// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
+
 //导入App跟组件
 import app from './App.vue'
 //按需导入mint-ui组件
-import { Header } from 'mint-ui';
+import { Header , Swipe, SwipeItem} from 'mint-ui';
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+/// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
 
-//导入MuI的样式
-import "./lib/mui/css/mui.min.css"
+
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
+
+
 var vm = new Vue({
     el:'#app',
-    render:c => c(app)
+    render:c => c(app),
+    router //1.4挂载路由对象到VM实例上
 })
